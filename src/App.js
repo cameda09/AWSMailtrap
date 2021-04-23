832c94a9ffa7918563ddec8380d22ccb560bcbc7
cameda09 <david,cameron@curtin.edu.au>
2021-04-23 12:48:34 +0800

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+  fetch("https://mailtrap.io/api/v1/inboxes/653043/messages", {
+    "method": "GET",
+    "headers": {
+        "Authorization": "Bearer a7281cf0ba20007ecf510de136c28c82",
+        "Access-Control-Allow-Origin": "http://localhost:3000"
+    }
+});
+// axios.get('https://mailtrap.io/api/v1/inboxes/653043/messages', {
+//   headers: {
+//     'Authorization': "Bearer a7281cf0ba20007ecf510de136c28c82"
+//   }
+// })
+// .then((res) => {
+//   console.log(res.data)
+// })
+// .catch((error) => {
+//   console.error(error)
+// })
+
   return (
     <div className="App">
       <header className="App-header">
