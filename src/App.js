a880bda3062ccd0ded6c1646ba6457092a7725f6
cameda09 <david,cameron@curtin.edu.au>
2021-05-05 08:06:36 +0800

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,30 @@
-import logo from './logo.svg';
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import axios from 'axios';
-function App() {
-  fetch("https://mailtrap.io/api/v1/inboxes/653043/messages", {
-    "method": "GET",
-    "headers": {
-        "Authorization": "Bearer a7281cf0ba20007ecf510de136c28c82",
-        "Access-Control-Allow-Origin": "http://localhost:3000"
-    }
-});
-// axios.get('https://mailtrap.io/api/v1/inboxes/653043/messages', {
-//   headers: {
-//     'Authorization': "Bearer a7281cf0ba20007ecf510de136c28c82"
-//   }
-// })
-// .then((res) => {
-//   console.log(res.data)
-// })
-// .catch((error) => {
-//   console.error(error)
-// })
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React from scratch
-        </a>
-      </header>
+axios.get('https://mailtrap.io/api/v1/inboxes/653043/messages/2183724950/body.htmlsource?api_token=a7281cf0ba20007ecf510de136c28c82',{
+  params:{
+    Host: 'mailtrap.io',
+    Authorization: 'Bearer a7281cf0ba20007ecf510de136c28c82'
+  }
+})
+.then(function response(response) {
+  console.log(response);
+})
+class App extends Component {
+render(){
+  return(
+    <div>
+      <h1>
+        Hello World
+      </h1>
     </div>
   );
 }
+  }
 
+
+
+ 
 export default App;
+
